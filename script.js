233584d795d38af7db735b4d6245275ea4617b3a
adilmahar <adiltmahar@gmail.com>
2025-08-12 21:18:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Hello World")
+
+
+
+
+let user;
+let password;
+
+
+console.log(user)
+
+
+
+document.getElementById("subm").onclick = function(){
+    user = document.getElementById("Username").value
+    password = document.getElementById("Password").value
+
+    document.getElementById("PrankText").textContent = "You Have Been Hacked >:)"
+    document.getElementById("UserInputuser").textContent = `Your Username is: ${user}`
+    console.log(`[Log]: Userinput is`, user)
+
+
+    document.getElementById("UserInputPassword").textContent = `Yours Password is: ${password}`
+    console.log(`[Log]: Userinput is`, password)
+}
